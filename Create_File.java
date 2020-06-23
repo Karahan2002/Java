5bb29a1e222ffd41bec1d8ff13281420bee6e661
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-06-23 13:48:31 +0200

    Create Create_File.java

---
+++ Create_File.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileWriter;
+
+public class main{
+        public static void main(String[]args){
+                File file = new File("yakup.txt");
+                try{
+                        if(file.createNewFile()){
+                                System.out.println("dosya olusturuldu");
+                        }else{
+                                System.out.println("dosya olusturulamadi");
+                        }
+                }catch(Exception e){
+                        System.out.println("hata : "+e);
+                }
+                try{
+                        FileWriter writer = new FileWriter(file);
+                        writer.write("yakup karahan");
+                        writer.close();
+                        System.out.println("dosya yazildi");
+                }catch(Exception e){
+                        System.out.println("hata : "+e);
+                }
+        }
+}
