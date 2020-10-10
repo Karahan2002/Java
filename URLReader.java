a4c8b6e1a71387f515d0b9997054a2cc30dd78b9
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-10-10 03:38:45 +0200

    Update URLReader.java

--- URLReader.java
+++ URLReader.java
@@ -1,14 +1,14 @@
 import java.net.*;
 import java.io.*;
 
-public class main {
+public class URLReader {
     public static void main(String[] args) throws Exception {
 
         URL oracle = new URL("a url");
-        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(oracle.openStream()));
 
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
+        while ((inputLine = bf.readLine()) != null)
             System.out.println(inputLine);
         in.close();
     }
