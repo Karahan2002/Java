2866c6508b565248954e1f654b4261cb7f714115
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-10-18 16:50:17 +0200

    Create ReadInteger.java

---
+++ ReadInteger.java
@@ -0,0 +1,8 @@
+public class ReadInteger{
+        public static void main(String[]args){
+                int[] number = {1,2,3,4,5,6,7,8,9,10};
+                for(int x : number){
+                        System.out.println("The count is : "+x);
+                }
+        }
+}
