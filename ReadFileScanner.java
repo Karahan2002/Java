d75ece502caf92b9acc0b089c98eff030c35d377
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-11-26 11:49:55 +0100

    Update ReadFileScanner.java

--- ReadFileScanner.java
+++ ReadFileScanner.java
@@ -1,21 +1,18 @@
-import java.io.*;
+eimport java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
 
-public class ReadFileScanner {
-    public static void main(String[] args) throws IOException {
-
-        Scanner s = null;
-
-        try {
-            s = new Scanner(new BufferedReader(new FileReader("yakup.txt")));
-
-            while (s.hasNext()) {
-                System.out.println(s.next());
-            }
-        } finally {
-            if (s != null) {
-                s.close();
-            }
+public class main{
+        public static void main(String[]args)throws IOException{
+                try{
+                        Scanner scanner = new Scanner(new BufferedReader(new FileReader(new File("yakup.txt"))));
+                        while(scanner.hasNext()){
+                                System.out.print(scanner.next()+" ");
+                        }
+                }catch(Exception e){
+                        System.out.println("hata : "+e);
+                }
         }
-    }
 }
