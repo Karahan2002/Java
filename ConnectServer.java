380f7babe813feda5017dc335915b3ea8b56be70
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2021-03-11 18:08:00 +0100

    Create ConnectServer.java

---
+++ ConnectServer.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.net.*;
+
+public class ConnectServer{
+        public static void main(String[]args)throws IOException{
+                Socket s = new Socket("localhost",4999);
+        }
+}
